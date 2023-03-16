d88c592d0677539d871b253d8b389afbedca9f0e
Your Name <johndoe@example.com>
2023-03-16 16:10:27 -0300

    API Node.js

--- scripts.js
+++ scripts.js
@@ -1,34 +1,42 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
-const dolar = 5.2;
-const btc = 102110.8;
-const euro = 5.15; 
 
-const convertValues = () => {
+
+const convertValues = async () => {
     const inputReais = document.getElementById("input-real").value
     const realValueText = document.getElementById("real-value-text")
     const currencyValueText = document.getElementById("currency-value-text")
 
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    console.log(data)
+
+    const dolar = data.USDBRL.high;
+    const btc = data.BTCBRL.high;
+    const euro = data.EURBRL.high;
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
         { style: 'currency', currency: 'BRL' }
     ).format(inputReais);
-    
+
     if (select.value === 'US$ Dólar americano') {
-    currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
-        { style: 'currency', currency: 'USD' }
-    ).format(inputReais / dolar); }
-    
-   
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'USD' }
+        ).format(inputReais / dolar);
+    }
+
+
     if (select.value === 'Bitcoin') {
-    currencyValueText.innerHTML = new Intl.NumberFormat('en-WO',
-        { style: 'currency', currency: 'BTC' }
-    ).format(inputReais / btc); }
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-WO',
+            { style: 'currency', currency: 'BTC' }
+        ).format(inputReais / btc);
+    }
 
     if (select.value === '€ Euro') {
         currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
             { style: 'currency', currency: 'EUR' }
-        ).format(inputReais / euro); }
+        ).format(inputReais / euro);
+    }
 
 }
 
@@ -49,8 +57,8 @@ changeCurrency = () => {
     if (select.value === 'Bitcoin') {
         currencyName.innerHTML = "BTC"
         currencyImg.src = "img/btc.png"
-    }  
-    convertValues ()
+    }
+    convertValues()
 }
 
 button.addEventListener('click', convertValues)
